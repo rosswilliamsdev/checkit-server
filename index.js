10d505ab17eeedb12eac3b4e8e4346123713ffb6
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2025-06-12 12:04:54 -0500

    Bugfix CORS origin and credentials for production

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const db = require("./db");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://checkitplanner.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/projects", projectsRoutes);
